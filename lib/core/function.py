c8dc22247e5b1eb87ac9c97c14500171eb2fafd6
arvind <arvind.m.vepa@gmail.com>
2019-12-16 20:12:19 -0800

    remove l1 loss

--- lib/core/function.py
+++ lib/core/function.py
@@ -19,7 +19,7 @@ from core.evaluate import accuracy
 from core.inference import get_final_preds
 from utils.transforms import flip_back
 from utils.vis import save_debug_images
-from sklearn.metrics import mean_absolute_error
+#from sklearn.metrics import mean_absolute_error
 
 
 logger = logging.getLogger(__name__)
@@ -31,7 +31,7 @@ def train(config, train_loader, model, criterion, optimizer, epoch,
     data_time = AverageMeter()
     losses = AverageMeter()
     acc = AverageMeter()
-    l1_loss = AverageMeter()
+    # l1_loss = AverageMeter()
 
     # switch to train mode
     model.train()
@@ -65,11 +65,11 @@ def train(config, train_loader, model, criterion, optimizer, epoch,
         # measure accuracy and record loss
         losses.update(loss.item(), input.size(0))
 
-        print(target.detach().cpu().numpy().shape)
-        print(output.detach().cpu().numpy().shape)
+        #print(target.detach().cpu().numpy().shape)
+        #print(output.detach().cpu().numpy().shape)
 
-        l1_loss_output = mean_absolute_error(target.detach().cpu().numpy(), output.detach().cpu().numpy())
-        l1_loss.update(l1_loss_output, cnt)
+        #l1_loss_output = mean_absolute_error(target.detach().cpu().numpy(), output.detach().cpu().numpy())
+        #l1_loss.update(l1_loss_output, cnt)
 
         _, avg_acc, cnt, pred = accuracy(output.detach().cpu().numpy(),
                                          target.detach().cpu().numpy())
@@ -85,11 +85,10 @@ def train(config, train_loader, model, criterion, optimizer, epoch,
                   'Speed {speed:.1f} samples/s\t' \
                   'Data {data_time.val:.3f}s ({data_time.avg:.3f}s)\t' \
                   'Loss {loss.val:.5f} ({loss.avg:.5f})\t' \
-                  'Accuracy {acc.val:.3f} ({acc.avg:.3f})\t' \
-                  'L1 Loss {l1_loss.val:.3f} ({l1_loss.avg:.3f})'.format(
+                  'Accuracy {acc.val:.3f} ({acc.avg:.3f})'.format(
                       epoch, i, len(train_loader), batch_time=batch_time,
                       speed=input.size(0)/batch_time.val,
-                      data_time=data_time, loss=losses, acc=acc, l1_loss=l1_loss)
+                      data_time=data_time, loss=losses, acc=acc)
             logger.info(msg)
 
             writer = writer_dict['writer']
@@ -108,7 +107,7 @@ def validate(config, val_loader, val_dataset, model, criterion, output_dir,
     batch_time = AverageMeter()
     losses = AverageMeter()
     acc = AverageMeter()
-    l1_loss = AverageMeter()
+    #l1_loss = AverageMeter()
 
     # switch to evaluate mode
     model.eval()
@@ -163,8 +162,8 @@ def validate(config, val_loader, val_dataset, model, criterion, output_dir,
             # measure accuracy and record loss
             losses.update(loss.item(), num_images)
 
-            l1_loss_output = mean_absolute_error(target.cpu().numpy(), output.cpu().numpy())
-            l1_loss.update(l1_loss_output, cnt)
+            # l1_loss_output = mean_absolute_error(target.cpu().numpy(), output.cpu().numpy())
+            # l1_loss.update(l1_loss_output, cnt)
 
             _, avg_acc, cnt, pred = accuracy(output.cpu().numpy(),
                                              target.cpu().numpy())
@@ -197,9 +196,8 @@ def validate(config, val_loader, val_dataset, model, criterion, output_dir,
                 msg = 'Test: [{0}/{1}]\t' \
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t' \
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t' \
-                      'Accuracy {acc.val:.3f} ({acc.avg:.3f})\t' \
-                      'L1 Loss {l1_loss.val:.3f} ({l1_loss.avg:.3f})'.format(i, len(val_loader), batch_time=batch_time,
-                                                                             loss=losses, acc=acc, l1_loss=l1_loss)
+                      'Accuracy {acc.val:.3f} ({acc.avg:.3f})'.format(i, len(val_loader), batch_time=batch_time,
+                                                                      loss=losses, acc=acc)
                 logger.info(msg)
 
                 prefix = '{}_{}'.format(
